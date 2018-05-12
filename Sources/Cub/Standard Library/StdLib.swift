afd1cb374da19ac1f60a343a15a0c91733a3e62f
Louis D'hauwe <louisdhauwe@silverfox.be>
2018-05-12 21:41:47 +0200

    Add unescaped String util

--- Sources/Cub/Standard Library/StdLib.swift
+++ Sources/Cub/Standard Library/StdLib.swift
@@ -404,3 +404,18 @@ public class StdLib {
 	}
 
 }
+
+public extension String {
+	
+	public var unescaped: String {
+		let entities = ["\0", "\t", "\n", "\r", "\"", "\'", "\\"]
+		var current = self
+		for entity in entities {
+			let descriptionCharacters = entity.debugDescription.dropFirst().dropLast()
+			let description = String(descriptionCharacters)
+			current = current.replacingOccurrences(of: description, with: entity)
+		}
+		return current
+	}
+	
+}
