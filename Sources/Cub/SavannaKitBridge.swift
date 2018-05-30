4a593b78ec570d6ce95fb3a550160c81700a5e32
Louis D'hauwe <louisdhauwe@silverfox.be>
2018-05-30 19:01:01 +0200

    Update SavannaKit bridge

--- Sources/Cub/SavannaKitBridge.swift
+++ Sources/Cub/SavannaKitBridge.swift
@@ -127,8 +127,10 @@ public struct DefaultTheme: SyntaxColorTheme {
 		return Color(red: 100/255, green: 100/255, blue: 100/255, alpha: 1.0)
 	}
 	
-	public let lineNumbersStyle: LineNumbersStyle? = LineNumbersStyle(font: Font(name: "Menlo", size: 16)!, textColor: lineNumbersColor, backgroundColor: Color(red: 21/255.0, green: 22/255, blue: 31/255, alpha: 1.0))
+	public let lineNumbersStyle: LineNumbersStyle? = LineNumbersStyle(font: Font(name: "Menlo", size: 16)!, textColor: lineNumbersColor)
 	
+	public let gutterStyle: GutterStyle = GutterStyle(backgroundColor: Color(red: 21/255.0, green: 22/255, blue: 31/255, alpha: 1.0), minimumWidth: 32)
+
 	public let font = Font(name: "Menlo", size: 15)!
 	
 	public let backgroundColor = Color(red: 31/255.0, green: 32/255, blue: 41/255, alpha: 1.0)
